834afb134d6c8785383598332b8873fa7b80d180
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-18 10:46:01 -0500

    adding

---
+++ 03_product_list.sql..sql
@@ -0,0 +1,7 @@
+use northwind;
+
+select productID, productName, unitprice 
+
+from products
+
+order by UnitPrice
---
+++ 04_product_list.sql..sql
@@ -0,0 +1,7 @@
+use northwind;
+
+select productID, productName, unitprice 
+
+from products
+
+where UnitPrice <= 7.5
